2292e27be83c8776743afdef86f2a1fe212960d8
janodin <japatundog@usm.edu.ph>
2024-03-28 00:05:50 +0800

    .

--- app/urls.py
+++ app/urls.py
@@ -3,4 +3,11 @@ from .views import *
 
 urlpatterns = [
     path('', home_page, name='home'),
+    path('about', about, name='about'),
+    path('editing', editing, name='editing'),
+    path('publishing', publishing, name='publishing'),
+    path('design', design, name='design'),
+    path('marketing', marketing, name='marketing'),
+    path('blog', blog, name='blog'),
+    path('contact', contact, name='contact'),
 ]
--- app/views.py
+++ app/views.py
@@ -3,3 +3,31 @@ from django.shortcuts import render, redirect
 
 def home_page(request):
     return render(request, 'iRiseup.html')
+
+
+def about(request):
+    return render(request, 'about.html')
+
+
+def editing(request):
+    return render(request, 'editing.html')
+
+
+def publishing(request):
+    return render(request, 'publishing.html')
+
+
+def design(request):
+    return render(request, 'design.html')
+
+
+def marketing(request):
+    return render(request, 'marketing.html')
+
+
+def blog(request):
+    return render(request, 'bloglist.html')
+
+
+def contact(request):
+    return render(request, 'contact.html')
