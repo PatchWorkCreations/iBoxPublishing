e030fb1b39ac44c3fcdba6dead815c2eb2f16b41
janodin <japatundog@usm.edu.ph>
2024-04-01 16:35:40 +0800

    .

--- app/views.py
+++ app/views.py
@@ -25,9 +25,125 @@ def marketing(request):
     return render(request, 'marketing.html')
 
 
-def blog(request):
+def bloglist(request):
     return render(request, 'bloglist.html')
 
 
 def contact(request):
     return render(request, 'contact.html')
+
+
+def all_events(request):
+    return render(request, 'all_events.html')
+
+
+def audiobook(request):
+    return render(request, 'audiobook.html')
+
+
+def autobiography(request):
+    return render(request, 'autobiography.html')
+
+
+def biography(request):
+    return render(request, 'biography.html')
+
+
+def blog_left_sidebar(request):
+    return render(request, 'blog-left-sidebar.html')
+
+
+def blog_list(request):
+    return render(request, 'blog-list.html')
+
+
+def blog_right_sidebar(request):
+    return render(request, 'blog-right-sidebar.html')
+
+
+def book_trailer(request):
+    return render(request, 'booktrailer.html')
+
+
+def childrens_book(request):
+    return render(request, 'childrensbook.html')
+
+
+def comic_book(request):
+    return render(request, 'comicbook.html')
+
+
+def horror_writing(request):
+    return render(request, 'horrorwriting.html')
+
+
+def index(request):
+    return render(request, 'index.html')
+
+
+def index_3(request):
+    return render(request, 'index-3.html')
+
+
+def index_4(request):
+    return render(request, 'index-4.html')
+
+
+def index_5(request):
+    return render(request, 'index-5.html')
+
+
+def index_6(request):
+    return render(request, 'index-6.html')
+
+
+def left_sidebar(request):
+    return render(request, 'left_sidebar.html')
+
+
+def manuscript(request):
+    return render(request, 'manuscript.html')
+
+
+def memoir(request):
+    return render(request, 'memoir.html')
+
+
+def non_fiction(request):
+    return render(request, 'nonfiction.html')
+
+
+def page_left_sidebar(request):
+    return render(request, 'page-left-sidebar.html')
+
+
+def page_right_sidebar(request):
+    return render(request, 'page-right-sidebar.html')
+
+
+def page_without_sidebar(request):
+    return render(request, 'page-without-sidebar.html')
+
+
+def privacy_policy(request):
+    return render(request, 'privacypolicy.html')
+
+
+def right_sidebar(request):
+    return render(request, 'right-sidebar.html')
+
+
+def screen_writing(request):
+    return render(request, 'screenwriting.html')
+
+
+def single_blog(request):
+    return render(request, 'single-blog.html')
+
+
+def single_event(request):
+    return render(request, 'single-event.html')
+
+
+def terms_and_condition(request):
+    return render(request, 'termsandcondition.html')
