d417d81471b264918ad21d71243da00f839f8488
PatchWorkCreations <153284133+PatchWorkCreations@users.noreply.github.com>
2024-05-04 17:21:02 +0800

    change

--- iBoxPublishing/settings.py
+++ iBoxPublishing/settings.py
@@ -133,5 +133,5 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = 'iRiseup Publishing Admin'
-EMAIL_HOST_USER = 'feed.teach.love@gmail.com'  # Your Gmail email address
-EMAIL_HOST_PASSWORD = 'crxv azjt vtzr jfdw'  # Your Gmail app password or regular password
+EMAIL_HOST_USER = 'iriseuppublishing@gmail.com'  # Your Gmail email address
+EMAIL_HOST_PASSWORD = 'qssn droz qmll flaz'  # Your Gmail app password or regular password
