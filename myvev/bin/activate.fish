e5d7ffbd0c71863a374187a113f5c41341b723bd
PatchWorkCreations <153284133+PatchWorkCreations@users.noreply.github.com>
2024-05-04 12:38:15 +0800

    change

--- myvev/bin/activate.fish
+++ myvev/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/Julia/Downloads/iBoxPublishing/myvev"
+set -gx VIRTUAL_ENV "/Users/Clem/Downloads/iBoxPublishing/myvev"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
